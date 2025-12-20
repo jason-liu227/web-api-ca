d2f3d0e8febad5be53b84fdafc249f7922ee5e04
JasonLiu <jasonliudehui227@gmail.com>
2025-12-20 02:57:05 +0000

    Added images api and router

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { getMovies, getMovie, getUpcomingMovies} from '../tmdb-api'; 
+import { getMovies, getMovie, getUpcomingMovies, getMovieImages} from '../tmdb-api'; 
 
 const router = express.Router();
 
@@ -19,6 +19,13 @@ router.get('/:id', asyncHandler(async(req, res) => {
 router.get('/upcoming', asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
+}));
+
+router.get('/:id', asyncHandler(async(req, res) => {
+    const {id}= req.params;
+    const movieImages = await getMovieImages(id);
+     console.log('Backend received request for movie ID:', id);
+    res.status(200).json(movieImages);
 }));
 
 export default router;
--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -22,6 +22,17 @@ export const getMovie = async (id) =>{
     return await response.json();
 };
 
+export const getMovieImages = async (id) =>{
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.TMDB_KEY}`
+    );
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+    return await response.json();
+};
+
+
 export const getUpcomingMovies = async () => {
     const response = await fetch(
         `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
