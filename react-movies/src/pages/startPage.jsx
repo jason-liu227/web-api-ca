11234af1249876f8f77f3519378b060916d77757
JasonLiu <jasonliudehui227@gmail.com>
2025-12-21 01:32:01 +0000

    Updated startPage.jsx to use authentication to change display

--- react-movies/src/pages/startPage.jsx
+++ react-movies/src/pages/startPage.jsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router";
+import { useContext } from "react";
+import { AuthContext } from '../contexts/authContext';
 
 const StartPage = () => {
-  
-    return(
-        <>
+    const context = useContext(AuthContext);
+
+    return context.isAuthenticated ? (
             <p>
-                Welcome to Movies App! View your <Link to="/discover">Movies</Link> or your <Link to="/profile">Profile</Link>.
+                Welcome {context.userName}! View your <Link to="/discover">Movies</Link> or your <Link to="/profile">Profile</Link>.
             </p>
+            ) : (
             <p>
                 <Link to="/login">Login</Link> or <Link to="/signup">Signup</Link> to view Movies!
             </p>
-        </>
-    );
+        );
   };
 
 export default StartPage;
